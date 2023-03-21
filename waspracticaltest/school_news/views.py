f3af1e4049e9a7d5337778400b463d50ca1b94de
Darcey Haddow <2731598h@student.gla.ac.uk>
2023-03-21 16:30:22 +0000

    standard requirements met

--- waspracticaltest/school_news/views.py
+++ waspracticaltest/school_news/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from school_news.models import Page
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': 'School News'}
+    page_list = Page.objects.all()
+
+    context_dict = {}
+    context_dict['pages'] = page_list
     return render(request, 'school_news/index.html', context=context_dict)
